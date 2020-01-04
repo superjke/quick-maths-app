c49fdb5a3ddf5d9fb725b660ab3bc7f7f0c90098
superjke <jordan.evans@mail.com>
2020-01-04 14:09:20 +0000

    Added out of time message

--- lib/widgets/end_game.dart
+++ lib/widgets/end_game.dart
@@ -4,8 +4,15 @@ class EndGame extends StatelessWidget {
   final int _num1;
   final int _num2;
   final Function _restart;
+  /*
+  reason is used to flag why the game ended.
+  0 - Wrong answer
+  1 - Out of itme
+  */
+  final int _reason;
 
-  EndGame(this._num1, this._num2, this._restart);
+  EndGame(this._num1, this._num2, this._restart, this._reason);
+
 
   @override
   Widget build(BuildContext context) {
@@ -13,30 +20,39 @@ class EndGame extends StatelessWidget {
       alignment: Alignment.topCenter,
       child: Column(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(top: 40.0, bottom: 10),
-            child: Text(
-              "Incorrect",
-              style: TextStyle(
-                color: Theme.of(context).errorColor,
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          Text(
+           Padding(
+                  padding: const EdgeInsets.only(top: 40.0, bottom: 10),
+                  child: _reason == 0
+              ? Text(
+                    "Incorrect",
+                    style: TextStyle(
+                      color: Theme.of(context).errorColor,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ): Text(
+                    "Out of time!",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                )
+              ,
+          _reason == 0 ? Text(
             "The correct answer was",
             style: TextStyle(
               fontSize: 20,
             ),
-          ),
-          Text(
+          ) : Container(),
+          _reason == 0 ? Text(
             "${_num1 + _num2}",
             style: TextStyle(
               fontSize: 32,
               fontWeight: FontWeight.bold,
             ),
-          ),
+          ) : Container(),
           Padding(
             padding: const EdgeInsets.only(top: 70.0),
             child: IconButton(
