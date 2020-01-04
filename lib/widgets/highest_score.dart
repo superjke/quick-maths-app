48889f68022fb91d94f9cd8fa51d5ae72234c361
superjke <jordan.evans@mail.com>
2020-01-04 14:09:52 +0000

    Removed card style

--- lib/widgets/highest_score.dart
+++ lib/widgets/highest_score.dart
@@ -8,18 +8,14 @@ class HighestScore extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.only(top: 20, bottom: 15),
-      child: Card(
-        color: _highScore > 0 ? Colors.red : Colors.grey,
-        elevation: 5,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            'Best: $_highScore',
-            style: TextStyle(
-              fontSize: 26,
-              color: Colors.white,
-            ),
+      padding: const EdgeInsets.only(top: 20, bottom: 0),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          'Best: $_highScore',
+          style: TextStyle(
+            fontSize: 28,
+            color: Colors.black,
           ),
         ),
       ),
