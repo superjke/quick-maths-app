315a9ebb4e2d340cd4f9465f3335b6b305a9e8ec
superjke <jordan.evans@mail.com>
2020-01-04 14:59:09 +0000

    Added dynamic colours to countdown

--- lib/widgets/countdown.dart
+++ lib/widgets/countdown.dart
@@ -10,14 +10,14 @@ class Countdown extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.only(top: 10.0),
       child: CircleAvatar(
-        backgroundColor: Colors.black,
+        backgroundColor: _time < 4 ? Colors.red : _time < 7 ? Colors.yellow : Colors.green,
         radius: 30,
         child: Padding(
           padding: EdgeInsets.all(5),
           child: FittedBox(
               child: Text(
             _time.toString(),
-            style: TextStyle(fontSize: 26),
+            style: TextStyle(fontSize: 26, color: _time < 4 ? Colors.white :_time < 7 ? Colors.black : Colors.white),
           )),
         ),
       ),
